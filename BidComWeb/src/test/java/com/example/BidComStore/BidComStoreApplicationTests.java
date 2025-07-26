a21972d685e88794476804df3b5d42cf8e0826d5
RamiroRodriguezC <ramirorodcas1112@gmail.com>
2025-07-26 01:03:16 -0300

    Proyecto inicial

---
+++ BidComWeb/src/test/java/com/example/BidComStore/BidComStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BidComStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BidComStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
